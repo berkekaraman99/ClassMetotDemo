e93aa5ada7977f963340e106058fdd7ab67eef03
Berke Karaman <berkekaraman99@hotmail.com>
2021-02-06 14:08:38 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("{0} Id'li müşteri eklendi.", musteri.Id);
+        }
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Müşteri Id: " + musteriler[i].Id);
+                Console.WriteLine("Müşteri Adı: " + musteriler[i].Ad);
+                Console.WriteLine("Müşteri Soyadı: " + musteriler[i].Soyad);
+                Console.WriteLine("Müşteri Şehir: " + musteriler[i].Sehir);
+                Console.WriteLine("------------------------------");
+            }
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("{0} Id'li müşteri silindi.", musteri.Id);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Berke";
+            musteri1.Soyad = "Karaman";
+            musteri1.Sehir = "Istanbul";
+            musteriManager.MusteriEkle(musteri1);
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Ahmet";
+            musteri2.Soyad = "Demir";
+            musteri2.Sehir = "Ankara";
+            musteriManager.MusteriEkle(musteri2);
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Emir";
+            musteri3.Soyad = "Çevik";
+            musteri3.Sehir = "Izmir";
+            musteriManager.MusteriEkle(musteri3);
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            musteriManager.MusteriListele(musteriler);
+
+            musteriManager.MusteriSil(musteri2);
+
+            musteriler = new Musteri[] { musteri1, musteri3 };
+            musteriManager.MusteriListele(musteriler);
+
+        }
+    }
+}
